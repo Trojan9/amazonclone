e85f8608585a68a6423adf0fd67937f37440fc1e
Trojan9 <timmybanjo@gmail.com>
2020-08-11 23:12:45 +0100

    Updates

---
+++ precache-manifest.8d439c28f81c8b2d608ab3ea5765e477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7acdfa832a13d8991cebe94d44b88d4",
+    "url": "/amazonclone/index.html"
+  },
+  {
+    "revision": "0b578424494d687af437",
+    "url": "/amazonclone/static/css/main.ab43d1fe.chunk.css"
+  },
+  {
+    "revision": "3300f43ecef183936103",
+    "url": "/amazonclone/static/js/2.c9cc6007.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/amazonclone/static/js/2.c9cc6007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b578424494d687af437",
+    "url": "/amazonclone/static/js/main.095dcb90.chunk.js"
+  },
+  {
+    "revision": "66a55bc96af283f2e430",
+    "url": "/amazonclone/static/js/runtime-main.ea76d9b7.js"
+  }
+]);
